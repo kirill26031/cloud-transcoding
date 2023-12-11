8e9f39f042930794f32dfbe238a437baf48fcc38
kirill26031 <kirill26031@gmail.com>
2023-12-11 02:46:44 +0200

    Fix error

--- worker/src/main/java/VideoProcessor.java
+++ worker/src/main/java/VideoProcessor.java
@@ -60,7 +60,7 @@ public class VideoProcessor {
                         System.out.println("Downloaded video to " + downloadedVideo.toPath());
 
                         String newStorageKey = StorageService.generateRandomToken(128);
-                        File outputFile = new File(outputFolder.getAbsolutePath() + "/Fix " + newStorageKey);
+                        File outputFile = new File(outputFolder.getAbsolutePath() + "/" + newStorageKey);
                         boolean executionResult = CliUtils.executeVideoConversion(options,
                                 wrapInQuotes(downloadedVideo.getAbsolutePath()),
                                 wrapInQuotes(outputFolder.getAbsolutePath()));
